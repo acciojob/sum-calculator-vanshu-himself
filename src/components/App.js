12ea1d35954e6bf7e439938bc07319de88c4ea96
Vansh Gupta <you@example.com>
2023-07-21 09:26:10 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[count,setCount]=useState(0);
+  // function add(){
+  //   let sum=count;
+  //   setCount(sum+)
+  // }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Sum calculator</h1>
+        <input
+        type="number"
+        onChange={(e)=>setCount(parseInt(count)+parseInt(e.target.value))}
+        />
+        <p>Sum: {count} </p>
     </div>
   )
 }
